8135f3b8068ff3ae25cc30764e3b3a3e625b6169
Paloma Jurado <51180179+sambasilio@users.noreply.github.com>
2020-05-25 12:05:27 -0500

    Subiendo boilerplate

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import { example } from "./example.js";
 
 example();
---
+++ src/view/404.js
@@ -0,0 +1,12 @@
+export default () => {
+  const viewDifferent = `
+            <h2>404</h2>
+            <h1>Página no encontrada</h1>
+            <p>El archivo especificado no se encontró en este sitio web. Por favor, URL para errores y vuelva a intentarlo.</p>
+          `;
+
+  const divElemt = document.createElement("div");
+  divElemt.innerHTML = viewDifferent;
+  divElemt.setAttribute("id", "message");
+  return divElemt;
+};
