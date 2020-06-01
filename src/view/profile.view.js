d8b466f0f9c4a5edaa39dd71deab60fce45b454f
ElinAlice <nelializ02@gmail.com>
2020-05-31 21:13:53 -0500

    Style: Mejorando la vista de Profile e historias

--- src/view/profile.view.js
+++ src/view/profile.view.js
@@ -8,27 +8,30 @@ export default () => {
 			</div>
 		</header>
 		<section class="All-sections">
-			<section class="sectionFather">
-				<section class="sectionSon">
-					<div class="logo-principalMobile">
-						<img src="./assets/logo-limatopia.png" />
-					</div>
-					<a href="#/logIn"></a>
-					<section id="containerLogIn">
-					</section>
-				</section>
-			</section>
     <section class="container-Publications-Profile">
-      <section class="sectionProfile">
-        <div class="coverPage"> portad</div>
-        <div class="profilePicture">foto de Perfil</div>
+      <section class="sectionProfileCalendary">
+        <section class="sectionProfile">
+        <div class="coverPage">
+          <img src="http://lorempixel.com/400/200/" alt=""/>
+        </div>
+        <div class="profilePicture">
+          <img src="http://lorempixel.com/200/200/" alt=""/>
+          <h5>Nombre </h5>
+        </div>
+        </section>
       </section>
       <section class="sectionPublications" >
-        <div>
-          <input type="text">
-          <button class="publicar">publicar</button>
+        <div class="containerNewPublications">
+          <div class="commentary">¿Que quieres compartir?</div>
+          <textarea name="newCommentary" id="textAreaComentary" class="textAreaPublication" placeholder="Escribe" autofocus></textarea>
+          <div class="optionsToPost">
+            <img src="./assets/iconImage.png" alt="subirImagen" />
+            <button class="btnToPost"> Share </button>
+          </div>
         </div>
-        <div class="stories"> Historia de publicaciones</div>
+        <section id="containerStories" class="stories">
+          <a href="#/publications"></a>
+        </section>
       </section>
 		</section>
     <br>
--- src/view/publications.view.js
+++ src/view/publications.view.js
@@ -1,10 +1,17 @@
 /* eslint-disable quotes */
 export default () => {
   const viewPublications = `
-  <div class="authorPublication">Autor</div>
-  <div class="content">contenido publicacion</div>
-  <div class="interactions"> me gusta compartir</div>
-   `;
+    <div class="authorPublication">
+      <div class="detailsAuthor">
+        <img src="http://lorempixel.com/200/200/" alt=""/>
+        <h5>Nombre </h5>
+      </div>
+      <div id="toogleMenu">&#9776;</div>
+    </div>
+    <div class="content">contenido publicacion</div>
+    <div class="interactions"> 
+      <img src="./assets/corazon.png" alt="meGusta" />
+    </div> `;
 
   const divElement = document.createElement("div");
   divElement.innerHTML = viewPublications;
